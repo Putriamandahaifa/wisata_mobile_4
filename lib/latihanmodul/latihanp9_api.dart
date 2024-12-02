73e78298b41165e038b95e5a2c2d1bd1862dbc89
Putriamandahaifa <bayandj64@gmail.com>
2024-12-02 12:24:54 +0700

    Meal_API And Build apk

---
+++ lib/latihanmodul/latihanp9_api.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:lat_project/models/makanan_model.dart';
+import 'package:lat_project/services/makanan_service.dart';
+import 'package:lat_project/widgets/makanan_card.dart';
+
+class Pertemuan9APIScreen extends StatelessWidget {
+  final MakananService _makananService = MakananService();
+
+  Pertemuan9APIScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Pertemuan 9 API Excercise',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: FutureBuilder<List<MakananModel>>(
+        future: _makananService.fetchMakanan(),
+        builder: (context, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return Center(child: CircularProgressIndicator(),);
+          }else if(snapshot.hasError){
+            return Center(child: Text("Error: ${snapshot.error}"),);
+          } else if(!snapshot.hasData || snapshot.data!.isEmpty){
+            return Center(child: Text('Tidak Ada Makanan Yang Tersedia'),);
+          }else{
+            final makanan = snapshot.data!;
+            return ListView.builder(itemBuilder: (context, index){
+              return MakananCard(makananModel: makanan[index]);
+            });
+          }
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lat_project/latihanmodul/latihanp5.dart';
 import 'package:lat_project/latihanmodul/latihanp6.dart';
+import 'package:lat_project/latihanmodul/latihanp9_api.dart';
 import 'package:lat_project/screens/home_page.dart';
 import 'package:lat_project/screens/landing_page.dart';
 import 'package:lat_project/screens/splash_screen.dart';
@@ -14,7 +15,7 @@ class TravelApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: SplashScreen(),
     );
   }
 }
